ce77ac2b6e40026525d6245b2fcb4f282b4c2d6f
Alyson Silva <alyson.silva@gatecidos.net>
2018-11-06 13:59:32 -0300

    Primeiro Commit

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import {Frase} from '../shared/frase.model';
+
+export const FRASE: Frase[] = [
+    {fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender'},
+    {fraseEng: 'How are you?', frasePtBr: 'Como vai você?'},
+    {fraseEng: 'I watch TV', frasePtBr: 'Eu assisto TV'},
+    {fraseEng: 'I eat bread', frasePtBr: 'Eu como pão'}
+]; 
